a00b0601e102a8956428661177c2b83c1091e66b
Quynh Nguyen <quynhnguyen@Quynhs-Air.attlocal.net>
2022-01-07 13:22:03 -0800

    First commit

--- LeetCode/src/Matrix_74/log_mn.java
+++ LeetCode/src/Matrix_74/log_mn.java
@@ -6,22 +6,28 @@ public class log_mn {
             return false;
         }
 
-        int m = matrix.length;
         int n = matrix[0].length - 1;
 
-        int low = 0;
-        int high = matrix.length - 1;
+        int low = 0; //index of first sub-array
+        int high = matrix.length - 1; //index of last sub-array
 
         while (low <= high) {
-            int mid = (low + high) / 2;
+
+            int mid = (low + high) / 2; //find the middle array
+
+            // check if the first element of the mid array is the target
             if (matrix[mid][0] == target) {
                 return true;
             }
+            // check if the target is in the array and find the exact position of the target
             if (matrix[mid][0] < target && matrix[mid][n] >= target && binarySearch(matrix[mid], target)) {
                 return true;
-            } else if (matrix[mid][0] < target) {
+            }
+            //
+            else if (target > matrix[mid][0]) {
                 low = mid + 1;
-            } else {
+            }
+            else {
                 high = mid - 1;
             }
         }
@@ -29,19 +35,24 @@ public class log_mn {
     }
 
     public static boolean binarySearch(int[] nums, int myTarget) {
-        int left = 0;
-        int right = nums.length - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] == myTarget) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high){
+            int mid = (low+high)/2;
+            if (nums[mid] == myTarget){
                 return true;
-            } else if (nums[mid] < myTarget) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+            }
+            else if (myTarget < nums[mid]){
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
             }
         }
         return false;
-
     }
+
+    /*
+
+     */
 }
