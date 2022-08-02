8e8142a3033016b62e88d131833b3de92e298d4f
quynhpn2610 <quynhphuongnguyen.2610@gmail.com>
2022-08-03 00:13:53 +0700

    best day to sell stock

---
+++ LeetCode/src/Blind75/Array/BestDayToSellStock_121.java
@@ -0,0 +1,34 @@
+package Blind75.Array;
+
+import java.util.Scanner;
+
+public class BestDayToSellStock_121 {
+    public static int maxProfit(int[] prices) {
+        int l = 0;
+        int r = 1;
+        int maxP = 0;
+        int len = prices.length;
+
+        while (r < len){
+            if (prices[r] > prices[l]){
+                int profit = prices[r]-prices[l];
+                maxP = Math.max(maxP, profit);
+            }
+            else{
+                l = r;
+            }
+            r++;
+        }
+        return maxP;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(maxProfit(nums));
+    }
+}
