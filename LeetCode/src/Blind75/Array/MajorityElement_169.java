a45742fb64fe645066b6480babdf9a295b7b3b75
Quynh Nguyen <quynhpn2610@Quynhs-MacBook-Air.local>
2022-09-29 22:14:56 -0400

    majority element

---
+++ LeetCode/src/Blind75/Array/MajorityElement_169.java
@@ -0,0 +1,27 @@
+package Blind75.Array;
+
+import java.util.*;
+
+public class MajorityElement_169 {
+    public int majorityElement(int[] nums) {
+        int n = nums.length/2;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i<nums.length; i++){
+            if (map.containsKey(nums[i])){
+                map.put(nums[i], map.get(nums[i])+1);
+            }
+            else{
+                map.put(nums[i], 1);
+            }
+        }
+
+        for (int j:
+             map.keySet()){
+            if (map.get(j) > n){
+                return j;
+            }
+        }
+
+        return -1;
+    }
+}
