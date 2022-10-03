bd12a1189991dcbb39fbfec8965e845e95376178
Quynh Nguyen <quynhpn2610@Quynhs-MacBook-Air.local>
2022-10-03 14:35:09 -0400

    binary search

--- LeetCode/src/Binary_704/BinarySearch.java
+++ LeetCode/src/Binary_704/BinarySearch.java
@@ -2,6 +2,7 @@ package Binary_704;
 
 public class BinarySearch {
     public int search (int[] nums, int target){
+        if (nums.length == 1) return nums[0];
         int left_pointer = 0;
         int right_pointer = nums.length - 1;
         while (left_pointer <= right_pointer){
